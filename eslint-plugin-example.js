acdadbb0653ca5809e2b6da8404dc9cc9f683c87
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-04-07 10:44:53 +0900

    pluginの型定義

--- eslint-plugin-example.js
+++ eslint-plugin-example.js
@@ -1,5 +1,6 @@
 import noLiteral from "./src/rules/no-literal.js";
 
+/** @type { import("eslint").ESLint.Plugin } */
 const plugin = { rules: { "no-literal": noLiteral } };
 
 export default plugin;
