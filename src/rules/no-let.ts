69699780aaebe2099b993e599657d9ec6b8ebbe2
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-05-01 16:45:47 +0900

    no-letのfix実装

--- src/rules/no-let.ts
+++ src/rules/no-let.ts
@@ -1,6 +1,10 @@
 import { Rule } from "eslint";
 
 const rule: Rule.RuleModule = {
+  meta: {
+    type: "problem",
+    fixable: "code",
+  },
   create: (context) => {
     return {
       VariableDeclaration: (node) => {
@@ -9,6 +13,15 @@ const rule: Rule.RuleModule = {
         context.report({
           message: "そのlet大丈夫そう？",
           node,
+          fix: (fixer) => {
+            const firstToken = context.sourceCode.getFirstToken(
+              node,
+              (token) => token.value === "let"
+            );
+            if (!firstToken) throw new Error("firstToken not found.");
+
+            return fixer.replaceText(firstToken, "const");
+          },
         });
       },
     };
