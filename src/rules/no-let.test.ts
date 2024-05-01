1242130e14ea8aec43ddc5d724439db7f731e863
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-05-01 16:46:09 +0900

    no-letのUT追加

--- src/rules/no-let.test.ts
+++ src/rules/no-let.test.ts
@@ -8,8 +8,18 @@ tester.run("no-let", rule, {
   valid: [{ code: `const x = 1` }],
   invalid: [
     {
-      code: `let x = 1;`,
-      errors: [{ message: "そのlet大丈夫そう？" }],
+      code: `
+        let x = 1;
+        let y = 1;
+      `,
+      errors: [
+        { message: "そのlet大丈夫そう？", line: 2 },
+        { message: "そのlet大丈夫そう？", line: 3 },
+      ],
+      output: `
+        const x = 1;
+        const y = 1;
+      `,
     },
   ],
 });
