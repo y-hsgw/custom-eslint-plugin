fcb3fe9d47886cead4d1d2b7f0fbf70bb4b7d95d
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-05-06 15:56:42 +0900

    getTestNameの実装

---
+++ src/test/utils.ts
@@ -0,0 +1,13 @@
+import path from "path";
+
+export const getTestName = () => {
+  const error = new Error();
+  const stackLines = error.stack?.split("\n");
+  const callerLine = stackLines?.[2];
+  const match = callerLine?.match(/at (.*):\d+:\d+/);
+  if (!match || match.length < 2) {
+    throw new Error("呼び出し元のファイル名が取得できません");
+  }
+
+  return path.basename(match[1], ".test.ts");
+};
